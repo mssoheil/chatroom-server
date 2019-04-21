644b7f31034a42316aa6c0a2cc9db318e0b105e3
mssoheil <mssoheil88@yahoo.com>
2019-04-21 20:25:59 +0430

    port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 const appController = require("./app.js");
 
-const port = process.env.PORT || 1004;
+const port = process.env.PORT || 3000;
 
 //if (process.env.NODE_ENV === "production") {
 	app.use(express.static("/"));
