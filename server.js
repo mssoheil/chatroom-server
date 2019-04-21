7d30ef900af5ab3f0e22fef6f8c0f45c3d74f544
mssoheil <mssoheil88@yahoo.com>
2019-04-21 20:09:47 +0430

    static

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const appController = require("./app.js");
 
 const port = process.env.PORT || 1004;
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static("./../client/build"));
-}
+//if (process.env.NODE_ENV === "production") {
+	app.use(express.static("/"));
+//}
 
 const server = app.listen(port, () => {
 	if (process.env.NODE_ENV !== "production") {
